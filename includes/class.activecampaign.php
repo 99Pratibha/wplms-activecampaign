c04794985312289ccf758e27cd91cd9fdfebfcef
Pratibha <pratibha@99fusion.com>
2020-03-09 18:26:35 +0530

    print_r removed

--- includes/class.activecampaign.php
+++ includes/class.activecampaign.php
@@ -110,7 +110,6 @@ class Wplms_Activecampaign{
 		$args['body'] = json_encode($contact_args);
 		$response = wp_remote_post(  $this->apiurl.'contacts',$args );
 		$body = json_decode( wp_remote_retrieve_body( $response ) );
-		
 		return $body->contact->id;
 	}
 
@@ -127,7 +126,6 @@ class Wplms_Activecampaign{
 		$args['body'] = json_encode($contact_args);
 		$response = wp_remote_post($this->apiurl.'contactLists',$args);
 		$body = json_decode(wp_remote_retrieve_body($response));
-		print_r('update contacts');
 		print_r($body);
 	}
 
